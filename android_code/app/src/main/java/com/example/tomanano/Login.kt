354035ed72b4a4007cc23057aca631abb1d4f54d
huni98asd <huni98asd@users.noreply.github.com>
2020-12-01 01:05:43 +0200

    Update login and register page

--- android_code/app/src/main/java/com/example/tomanano/Login.kt
+++ android_code/app/src/main/java/com/example/tomanano/Login.kt
@@ -1,11 +1,17 @@
 package com.example.tomanano
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        buttonLogToReg.setOnClickListener {
+            startActivity(Intent(this,Register::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
     }
 }
