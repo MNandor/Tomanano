33395c12d83b0e020740511d392a4fea09f4edaa
huni98asd <huni98asd@users.noreply.github.com>
2020-12-10 19:48:03 +0200

    Create Room

--- android_code/app/src/main/java/com/example/tomanano/MainActivity.kt
+++ android_code/app/src/main/java/com/example/tomanano/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.tomanano
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+    }
 
 
 
