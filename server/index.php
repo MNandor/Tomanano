d8f4234dfcc0675957b28ef3d7f5410243308a23
Norberto9 <Norberto9@users.noreply.github.com>
2020-12-10 19:56:58 +0200

    Creating method that returnes a list of rooms of a user

--- server/index.php
+++ server/index.php
@@ -36,6 +36,12 @@ Note! Each activity requires the <i>request_type</i> data that is actually the n
 	<font size="3">Required: user_email, room_id <br>
 	Response ex: [{"position":1,"email":"abc@gmail.com","score":"25"},{"position":2,"email":"kedves.norbert@student.ms.sapientia.ro","score":"14"}] <br> When failed: [] <br>
 	ex: <u>/result.php?request_type=6&user_email=kedves.norbert@student.ms.sapientia.ro&room_id=10</u></font>
+	<h2><li>Get roomlist</li></h2>
+	<font size="3">Required: user_email<br>
+	Response ex: [3,{"room_id":"2","room_name":"Szoba_2","start_date":"2020-01-01 20:22:00","end_date":"2020-12-01 20:22:00"},{"room_id":"10","room_name":"Szoba6001","start_date":"2020-11-01 20:22:00","end_date":"2020-12-01 20:22:00"},{"room_id":"12","room_name":"Sf7895","start_date":"2020-11-01 20:22:00","end_date":"2020-12-01 20:22:00"}]
+	<br> When failed: [] <br>
+	ex: <u>/roomlist.php?request_type=7&user_email=kedves.norbert@student.ms.sapientia.ro</u></font>
+	
 </ol>
 </body>
 
